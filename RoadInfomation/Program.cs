bf49fe810f6d32c1397075a08a947d6229020740
moguonyako <moguonyanko@gmail.com>
2025-05-29 09:24:04 +0900

    道路施設情報取得ツールのサンプルコードを追加し、橋梁情報取得メソッドを修正

--- RoadInfomation/Program.cs
+++ RoadInfomation/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * C#でModel Context Protocol (MCP)を使用して道路施設情報を取得するツールのサンプルコード
+ * 参考サイト:
+ * https://modelcontextprotocol.io/quickstart/server#c
+ */
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+using System.Net.Http.Headers;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddSingleton(_ =>
+{
+  var client = new HttpClient() { BaseAddress = new Uri("https://road-structures-db.mlit.go.jp/xROAD/api/v1") };
+  client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("my-road-tool", "1.0"));
+  return client;
+});
+
+var app = builder.Build();
+
+await app.RunAsync();
+
--- RoadInfomation/Tools/RoadTools.cs
+++ RoadInfomation/Tools/RoadTools.cs
@@ -8,22 +8,25 @@ namespace RoadInfomationServer.Tools;
 [McpServerToolType]
 public static class RoadTools
 {
-    [McpServerTool, Description("指定された経緯度を元に橋梁の情報を取得します。")]
-    public static async Task<string> GetBridgesByArea(
-        HttpClient client,
-        [Description("検索範囲を示す座標の配列です。書式は「北緯の下限,北緯の上限,東経の下限,東経の上限」です。")] double[] area)
+  [McpServerTool, Description("指定された経緯度を元に橋梁の情報を取得します。")]
+  public static async Task<string> GetBridgesByArea(
+      HttpClient client,
+      [Description("検索範囲を示す座標の配列です。書式は「北緯の下限,北緯の上限,東経の下限,東経の上限」です。")] double[] area)
+  {
+    var jsonElement = await client.GetFromJsonAsync<JsonElement>($"/bridges/{area[0]},{area[1]},{area[2]},{area[3]}");
+    var emptyResult = new
     {
-        var jsonElement = await client.GetFromJsonAsync<JsonElement>($"/bridges/{area[0]},{area[1]},{area[2]},{area[3]}");
-        if (jsonElement.ValueKind == JsonValueKind.Null)
-        {
-            return JsonSerializer.Serialize(new
-            {
-                code = "404",
-                message = "指定された範囲には橋梁情報が見つかりませんでした。"
-            });
-        }
-
-        return jsonElement.GetProperty("result").GetString();
+      code = "404",
+      message = "指定された範囲には橋梁情報が見つかりませんでした。"
+    };
+    if (jsonElement.ValueKind == JsonValueKind.Null)
+    {
+      return JsonSerializer.Serialize(emptyResult);
     }
+
+    var result = jsonElement.GetProperty("result").GetString();
+
+    return result ?? JsonSerializer.Serialize(emptyResult);
+  }
 }
 
